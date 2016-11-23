177805967defefba0ca8e6441d8ddf2a7a5c9467
unknown <>
2016-11-23 22:52:53 +0800

    add markdown support

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 var mongodb = require('./db');
+var markdown = require('markdown').markdown;
 
 function Post(name, title, post) {
     this.name = name;
@@ -85,6 +86,11 @@ Post.get = function(name, callback){
                 if(err){
                     return callback(err); //失败！返回err
                 }
+
+                //解析markdown为html
+                docs.forEach(function(doc){
+                    doc.post = markdown.toHTML(doc.post);
+                });
 
                 callback(null, docs); //成功！ 以数组形式返回查询结果
             });
