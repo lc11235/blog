f5cd3f60a88471dd5f1d461cd8cd2a99ba5abc3c
unknown <>
2016-12-07 23:09:28 +0800

    add app 1207

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log', {flag: 'a'});
+var errorLog = fs.createWriteStream('error.log', {flag: 'a'});
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -32,6 +35,8 @@ app.set('view engine', 'ejs');
 app.use(favicon(__dirname +'/public/favicon.ico'));
 //加载日志中间件,日志级别为dev
 app.use(logger('dev'));
+//将日志保存为文件
+app.use(logger({stream: accessLog}));
 //加载解析json的中间件
 app.use(bodyParser.json());
 //加载解析urlencoded请求体的中间件
@@ -40,6 +45,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //设置public文件夹为存放静态文件的目录。
 app.use(express.static(path.join(__dirname, 'public')));
+//添加一个中间件用来记录express的错误，记录在error.log文件中
+app.use(function(err, req, res, next){
+  var meta = '[' + new Date() + ']' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
+});
 //路由控制器
 //app.use('/', routes);
 //app.use('/users', users);
